ba37cc653f1502381dc99afe6a973b40187686fa
Alex <al.gr.0017@gmail.com>
2023-01-25 22:20:19 +0300

    feat: add solution for tests

--- src/routes/member-types/index.ts
+++ src/routes/member-types/index.ts
@@ -1,34 +1,54 @@
-import { FastifyPluginAsyncJsonSchemaToTs } from '@fastify/type-provider-json-schema-to-ts';
-import { idParamSchema } from '../../utils/reusedSchemas';
-import { changeMemberTypeBodySchema } from './schema';
-import type { MemberTypeEntity } from '../../utils/DB/entities/DBMemberTypes';
+import { FastifyPluginAsyncJsonSchemaToTs } from "@fastify/type-provider-json-schema-to-ts";
+import { idParamSchema } from "../../utils/reusedSchemas";
+import { changeMemberTypeBodySchema } from "./schema";
+import type { MemberTypeEntity } from "../../utils/DB/entities/DBMemberTypes";
 
 const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
   fastify
 ): Promise<void> => {
-  fastify.get('/', async function (request, reply): Promise<
+  fastify.get("/", async function (request, reply): Promise<
     MemberTypeEntity[]
-  > {});
+  > {
+    return await fastify.db.memberTypes.findMany();
+  });
 
   fastify.get(
-    '/:id',
+    "/:id",
     {
       schema: {
         params: idParamSchema,
       },
     },
-    async function (request, reply): Promise<MemberTypeEntity> {}
+    async function (request, reply): Promise<MemberTypeEntity> {
+      const data = await fastify.db.memberTypes.findOne({
+        key: "id",
+        equals: request.params.id,
+      });
+
+      if (data === null) throw reply.code(404);
+
+      return data;
+    }
   );
 
   fastify.patch(
-    '/:id',
+    "/:id",
     {
       schema: {
         body: changeMemberTypeBodySchema,
         params: idParamSchema,
       },
     },
-    async function (request, reply): Promise<MemberTypeEntity> {}
+    async function (request, reply): Promise<MemberTypeEntity> {
+      try {
+        return await fastify.db.memberTypes.change(
+          request.params.id,
+          request.body
+        );
+      } catch (error) {
+        throw reply.code(400);
+      }
+    }
   );
 };
 
